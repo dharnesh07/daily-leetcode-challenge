8b0b2f0073e58829ac82416fc82897346d34b6f1
DHARNESH KANNAN <dharneshsudha07@gmail.com>
2026-07-21 12:05:50 +0530

    Time: 1 ms (99.89%), Space: 55.8 MB (18.09%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,12 +2,17 @@ class Solution {
     public int majorityElement(int[] nums) {
         int candidate = 0;
         int count = 0;
+
+        // Phase 1: Find the candidate
         for (int num : nums) {
             if (count == 0) {
                 candidate = num;
             }
             count += (num == candidate) ? 1 : -1;
         }
+
+        // Phase 2: Since the problem guarantees a majority element,
+        // the candidate is the answer
         return candidate;
     }
 }
